b9f127926e9fb7e260d18c0fd7f7862ac33646fe
Nikhil Wadekar <nikhilrwadekar@gmail.com>
2020-02-04 07:12:56 -0800

    Added Global Variables/constants

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { APP_NAME } from "./resources/GlobalVariables";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>{APP_NAME}</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
