8a38267b2eaafdb72614a1d65077e80b408f3ad4
Nikhil Wadekar <nikhilrwadekar@gmail.com>
2020-02-04 11:22:34 -0800

    Installed Reactstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { APP_NAME } from "./resources/GlobalVariables";
+import TestComponent from "./components/Test/TestComponent";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>{APP_NAME}</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This App is called {APP_NAME}</h1>
+      <TestComponent />
     </div>
   );
 }
---
+++ src/components/Test/TestComponent.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import { Button } from "reactstrap";
+
+export default class TestComponent extends Component {
+  render() {
+    return <Button color="danger">Danger!</Button>;
+  }
+}
