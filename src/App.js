305fb42654deabb6c9be42255ab9d0e4cd8a5c15
Nikhil Wadekar <nikhilrwadekar@gmail.com>
2020-02-22 15:43:28 -0800

    Fixed Files + Added Login Route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,33 @@ import React from "react";
 import "./App.css";
 import { APP_NAME } from "./resources/GlobalVariables";
 import TestComponent from "./components/Test/TestComponent";
-
+import Button from "@material-ui/core/Button";
+import SignInPage from "./pages/SignInPage";
+import LandingPage from "./pages/LandingPage";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <h1>This App is called {APP_NAME}</h1>
-      <TestComponent />
+      <Router>
+        <Switch>
+          {/* Login Page Route */}
+          <Route path="/login">
+            <SignInPage />
+          </Route>
+
+          {/* Landing Page Route */}
+          <Route path="/">
+            <LandingPage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
