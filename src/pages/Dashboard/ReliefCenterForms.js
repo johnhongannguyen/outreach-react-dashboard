3d78122b1217447233d911184d850798d8045219
John <johhaneshongan@gmail.com>
2020-03-13 12:15:34 -0700

    Latest Commit for Relief Center form

--- src/pages/Dashboard/ReliefCenterForms.js
+++ src/pages/Dashboard/ReliefCenterForms.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
 import { FormControl, InputLabel, Select, MenuItem } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-
 import clsx from "clsx";
 import Radio from "@material-ui/core/Radio";
 import RadioGroup from "@material-ui/core/RadioGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import FormLabel from "@material-ui/core/FormLabel";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+// import "date-fns";
+// import Grid from "@material-ui/core/Grid";
+// import DateFnsUtils from "@date-io/date-fns";
+// import {
+//   MuiPickersUtilsProvider,
+//   KeyboardTimePicker,
+//   KeyboardDatePicker
+// } from "@material-ui/pickers";
 
 // Styles
 const useStyles = makeStyles({
@@ -52,6 +64,9 @@ const useStyles = makeStyles({
     }
   }
 });
+// const [selectedDate, setSelectedDate] = React.useState(
+//   new Date("2014-08-18T21:11:54")
+// );
 export default class ReliefCenterForms extends Component {
   constructor(props) {
     super(props);
@@ -68,6 +83,13 @@ export default class ReliefCenterForms extends Component {
   handleClick(event) {
     console.log(event.target);
   }
+  // handleDateChange = date => {
+  //   setSelectedDate(date);
+  // };
+
+  // selectedDate = ()=>{
+
+  // }
 
   StyledRadio = props => {
     const classes = useStyles();
@@ -104,42 +126,60 @@ export default class ReliefCenterForms extends Component {
             <MenuItem value={"Other"}> Other </MenuItem>
           </Select>
         </FormControl>
-
-        <FormControl id="volunteerTab">
-          <InputLabel id="volunteerDetail">Volunteer Details</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={nameOfJob}
-            onChange={this.handleChange}
-          >
-            <MenuItem value={"Driving"}> Driving </MenuItem>
-            <MenuItem value={"Baby Sitting"}> Baby Sitting </MenuItem>
-            <MenuItem value={"Cooking"}> Cooking </MenuItem>
-            <MenuItem value={"Other"}> Other </MenuItem>
-          </Select>
-          <FormControl id="availability" component="fieldset">
-            <FormLabel component="legend"></FormLabel>
-            <RadioGroup
-              defaultValue="anyTime"
-              aria-label="preference"
-              name="customized-radios"
+        <Card>
+          <FormControl id="volunteerTab">
+            <InputLabel id="volunteerDetail">Volunteer Details</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={nameOfJob}
+              onChange={this.handleChange}
             >
-              <FormControlLabel
-                value="anyTime"
-                control={<this.StyledRadio />}
-                label="Any Time"
-              />
-              <FormControlLabel
-                id="showDate"
-                onClick={this.handleClick}
-                value="preference"
-                control={<this.StyledRadio />}
-                label="Choose your preference"
-              />
-            </RadioGroup>
+              <MenuItem value={"Driving"}> Driving </MenuItem>
+              <MenuItem value={"Baby Sitting"}> Baby Sitting </MenuItem>
+              <MenuItem value={"Cooking"}> Cooking </MenuItem>
+              <MenuItem value={"Other"}> Other </MenuItem>
+            </Select>
+            <FormControl id="availability" component="fieldset">
+              <FormLabel component="legend"></FormLabel>
+              <RadioGroup
+                defaultValue="anyTime"
+                aria-label="preference"
+                name="customized-radios"
+              >
+                <FormControlLabel
+                  value="anyTime"
+                  control={<this.StyledRadio />}
+                  label="Any Time"
+                />
+                <FormControlLabel
+                  id="showDate"
+                  onClick={this.handleClick}
+                  value="preference"
+                  control={<this.StyledRadio />}
+                  label="Choose your preference"
+                />
+                {/* <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                  <Grid container justify="space-around">
+                    <KeyboardDatePicker
+                      disableToolbar
+                      variant="inline"
+                      format="MM/dd/yyyy"
+                      margin="normal"
+                      id="date-picker-inline"
+                      label="Date picker inline"
+                      value={this.selectedDate}
+                      onChange={this.handleDateChange}
+                      KeyboardButtonProps={{
+                        "aria-label": "change date"
+                      }}
+                    />
+                  </Grid>
+                </MuiPickersUtilsProvider> */}
+              </RadioGroup>
+            </FormControl>
           </FormControl>
-        </FormControl>
+        </Card>
       </>
     );
   }
