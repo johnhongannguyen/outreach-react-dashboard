8e59307859bde22b2313d33a1bbef2773b35ec25
Nikhil Wadekar <nikhilrwadekar@gmail.com>
2020-03-12 18:47:25 -0700

    Added Page to Assign Volunteers

---
+++ src/pages/Dashboard/ReliefCenters/AssignVolunteers.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+
+// Axios
+import axios from "axios";
+import { Paper, Grid, Typography, Button, Badge } from "@material-ui/core";
+import ReliefCenterActionCard from "../../../components/Dashboard/ReliefCenterActionCard";
+
+class AssignVolunteers extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default withStyles(AssignVolunteers);
