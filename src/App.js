12ba4bc45023d5c8cf80959a0e83363d236f8f24
Nikhil Wadekar <nikhilrwadekar@gmail.com>
2020-03-22 13:18:30 -0700

    Added Exact Prop

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
       <Router>
         <Switch>
           {/* Dashboard Route */}
-          <Route path="/dashboard">
+          <Route exact path="/dashboard">
             <Dashboard />
           </Route>
 
