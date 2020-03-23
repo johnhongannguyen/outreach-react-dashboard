58bcb9067b6ccb1b9d1ae15b31da9f62ea197e59
jasminekaur95 <kjasmine2810@gmail.com>
2020-03-22 21:07:29 -0700

    Added styles for Landing page

--- src/components/LandingPage/Features-Section/FeatureComponent.js
+++ src/components/LandingPage/Features-Section/FeatureComponent.js
@@ -13,7 +13,14 @@ const Features = ({ features }) => {
       // Container for each feature
       <div className="feature" key={feature.id}>
         {/* FEATURE ICON */}
-        <div>{feature.icon}</div>
+        <div className="feature-icon">
+          <img
+            src={feature.icon}
+            // width="400px"
+            className="feature-img"
+            viewBox="0 0 60 100"
+          />
+        </div>
 
         {/* FEATURE TITLE */}
         <div className="feature-title">{feature.title}</div>
--- src/components/LandingPage/Header/Header.js
+++ src/components/LandingPage/Header/Header.js
@@ -1,87 +1,87 @@
-import React from 'react';
-import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import List from '@material-ui/core/List';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import outreachlogo from '../images/outreach_logo.png';
-import Link from '@material-ui/core/Link';
-import Typography from '@material-ui/core/Typography';
-import { Button } from '@material-ui/core';
+import React from "react";
+import clsx from "clsx";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import List from "@material-ui/core/List";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import outreachlogo from "../images/outreach_logo.png";
+import Link from "@material-ui/core/Link";
+import Typography from "@material-ui/core/Typography";
+import { Button } from "@material-ui/core";
 import "./Header.css";
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
   root: {
-    display: 'flex',
+    display: "flex"
   },
   appBar: {
-    transition: theme.transitions.create(['margin', 'width'], {
+    transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
+      duration: theme.transitions.duration.leavingScreen
+    })
   },
   appBarShift: {
     width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['margin', 'width'], {
+    transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
+      duration: theme.transitions.duration.enteringScreen
     }),
-    marginRight: drawerWidth,
+    marginRight: drawerWidth
   },
   title: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   hide: {
-    display: 'none',
+    display: "none"
   },
   drawer: {
     width: drawerWidth,
-    flexShrink: 0,
+    flexShrink: 0
   },
   drawerPaper: {
-    width: drawerWidth,
+    width: drawerWidth
   },
   drawerHeader: {
-    display: 'flex',
-    alignItems: 'center',
+    display: "flex",
+    alignItems: "center",
     padding: theme.spacing(0, 1),
     // necessary for content to be below app bar
     ...theme.mixins.toolbar,
-    justifyContent: 'flex-start',
+    justifyContent: "flex-start"
   },
   content: {
     flexGrow: 1,
     padding: theme.spacing(3),
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
+      duration: theme.transitions.duration.leavingScreen
     }),
-    marginRight: -drawerWidth,
+    marginRight: -drawerWidth
   },
   contentShift: {
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
+      duration: theme.transitions.duration.enteringScreen
     }),
-    marginRight: 0,
-  },
+    marginRight: 0
+  }
 }));
 
- function Header() {
+function Header() {
   const classes = useStyles();
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
-  
+
   const preventDefault = event => event.preventDefault();
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -95,22 +95,21 @@ const useStyles = makeStyles(theme => ({
     <div className={classes.root}>
       <CssBaseline />
       <AppBar
-        style={{flexDirection: 'row', alignItems: 'center'}}
+        style={{ flexDirection: "row", alignItems: "center" }}
         position="absolute"
         className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
+          [classes.appBarShift]: open
         })}
       >
         <div class="logo">
-            <img
-                src={outreachlogo}
-                alt="Logo"
-                className="header-outreach-logo"
-                style={{width: "6rem"}}
-              />
-          </div>
-        <Toolbar style={{flex: "30% 1",
-            justifyContent: "flex-end"}}>
+          <img
+            src={outreachlogo}
+            alt="Logo"
+            className="header-outreach-logo"
+            style={{ width: "6rem" }}
+          />
+        </div>
+        <Toolbar style={{ flex: "30% 1", justifyContent: "flex-end" }}>
           <IconButton
             color="inherit"
             aria-label="open drawer"
@@ -118,30 +117,30 @@ const useStyles = makeStyles(theme => ({
             onClick={handleDrawerOpen}
             className={clsx(open && classes.hide)}
           >
-            <MenuIcon style={{fontSize: "3rem"}}/>
+            <MenuIcon style={{ fontSize: "3rem" }} />
           </IconButton>
         </Toolbar>
-        <div className='header-menu'>
+        <div className="header-menu">
           <Typography>
             <Link href="#" onClick={preventDefault}>
               Donate
             </Link>
-            <Link href="#" onClick={preventDefault} >
+            <Link href="#" onClick={preventDefault}>
               Team
             </Link>
             <Link href="#" onClick={preventDefault}>
               Contact
             </Link>
             <Button
-            size="lg"
-            style={{ backgroundColor: "#F27821", color: "#fff" }}
-            variant=""
-            className="nl-btn"
-        >
-                Download
-              </Button>
+              size="lg"
+              style={{ backgroundColor: "#F27821", color: "#fff" }}
+              variant=""
+              className="nl-btn"
+            >
+              Download
+            </Button>
           </Typography>
-          </div>
+        </div>
       </AppBar>
       <Drawer
         className={classes.drawer}
@@ -149,23 +148,27 @@ const useStyles = makeStyles(theme => ({
         anchor="right"
         open={open}
         classes={{
-          paper: classes.drawerPaper,
+          paper: classes.drawerPaper
         }}
       >
         <div className={classes.drawerHeader}>
           <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+            {theme.direction === "rtl" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
           </IconButton>
         </div>
         <List>
-          {['Donate', 'Team', 'Contact'].map((text, index) => (
+          {["Donate", "Team", "Contact"].map((text, index) => (
             <ListItem button key={text}>
               <ListItemText primary={text} />
             </ListItem>
           ))}
-          <ListItem button key={'Download'} class="button-download">
-              <ListItemText primary={'Download'} />
-            </ListItem>
+          <ListItem button key={"Download"} class="button-download">
+            <ListItemText primary={"Download"} />
+          </ListItem>
         </List>
       </Drawer>
     </div>
